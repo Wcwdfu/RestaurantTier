0c215d8dda69d2c09909cb9f5c869a11eb39d390
imjaehyeong <dlawogud517@gmail.com>
2024-01-03 09:08:29 +0900

    inital commit

---
+++ src/main/java/com/site/restauranttier/RestaurantTierApplication.java
@@ -0,0 +1,13 @@
+package com.site.restauranttier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantTierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantTierApplication.class, args);
+	}
+
+}
