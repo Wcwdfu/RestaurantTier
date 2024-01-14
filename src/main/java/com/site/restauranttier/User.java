595363ff32f03155826726db2406db182e3c8f32
imjaehyeong <dlawogud517@gmail.com>
2024-01-14 09:15:55 +0900

    entitiy deleted

--- src/main/java/com/site/restauranttier/User.java
+++ src/main/java/com/site/restauranttier/User.java
@@ -11,14 +11,14 @@ public class User {
     private Long userId;
 
     private String userPassword;
-//    private Integer userNum;
-//    private String userName;
-//    private String userSex;
-//    private Date userBirthday;
-//    private String userEmail;
-//    private String userNickname;
-//    private String status;
-//    private Date createdAt;
-//    private Date updatedAt;
+    private Integer userNum;
+    private String userName;
+    private String userSex;
+    private Date userBirthday;
+    private String userEmail;
+    private String userNickname;
+    private String status;
+    private Date createdAt;
+    private Date updatedAt;
 }
 
