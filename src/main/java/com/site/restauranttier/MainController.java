3a87f48c60c61fc0ecf2f94a643eff0e6b20669d
imjaehyeong <dlawogud517@gmail.com>
2024-01-15 16:50:50 +0900

    login completed

--- src/main/java/com/site/restauranttier/MainController.java
+++ src/main/java/com/site/restauranttier/MainController.java
@@ -7,17 +7,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Controller
 public class MainController {
     @Autowired
     private RestaurantRepository restaurantRepository;
-
+    @Autowired
+    private UserRepository userRepository;
     @GetMapping("/")
     public String root() {
         return "redirect:/home";
@@ -34,7 +36,7 @@ public class MainController {
     }
 
     @GetMapping("/main/tier")
-    public String tier(Model model, @RequestParam(name="cuisine",required = false) String cuisine) {
+    public String tier(Model model, @RequestParam(name = "cuisine", required = false) String cuisine) {
         List<Restaurant> restaurants;
         if (cuisine != null && !cuisine.isEmpty()) {
             // cuisine 파라미터가 주어진 경우, 해당하는 데이터를 조회합니다.
@@ -43,18 +45,19 @@ public class MainController {
             // cuisine 파라미터가 없는 경우, 모든 레스토랑을 조회합니다.
             restaurants = restaurantRepository.findAll();
         }
-        model.addAttribute("restaurants",restaurants);
+        model.addAttribute("restaurants", restaurants);
         return "tier";
     }
 
 
     @GetMapping("/tier")
-    public String tier(){
+    public String tier() {
         return "tier";
     }
+
     @ResponseBody
     @GetMapping("/api/tier")
-    public  ResponseEntity<List<Restaurant>> getRestaurantsByCuisine(@RequestParam(name = "cuisine", required = false) String cuisine) {
+    public ResponseEntity<List<Restaurant>> getRestaurantsByCuisine(@RequestParam(name = "cuisine", required = false) String cuisine) {
         List<Restaurant> restaurants;
         if (cuisine != null && !cuisine.isEmpty()) {
             // cuisine 파라미터가 주어진 경우, 해당하는 데이터를 조회합니다.
@@ -77,4 +80,37 @@ public class MainController {
         return "ranking";
     }
 
+    @GetMapping("/login/callback")
+    public String auth() {
+        return "callback";
+    }
+    @ResponseBody
+    @PostMapping("/api/user")
+    public ResponseEntity<?> userCreated(@RequestBody Map<String,String> userData){
+        String userId = userData.get("userId");
+        String userEmail = userData.get("userEmail");
+        String userNickname=userData.get("userNickname");
+
+        Optional<User> userOptional = userRepository.findById(userId);
+        
+        // 기존 사용자면 정보 업데이트
+        if(userOptional.isPresent()){
+            User user=userOptional.get();
+            user.setUserEmail(userEmail);
+            user.setUserNickname(userNickname);
+            user.setUpdatedAt(LocalDateTime.now());
+            userRepository.save(user);
+        }
+        // 새로운 사용자면 user 객체 만들고 db에 저장
+        else{
+        User newUser = new User();
+        newUser.setUserId(userId);
+        newUser.setCreatedAt(LocalDateTime.now());
+        newUser.setUserEmail(userEmail);
+        newUser.setUserNickname(userNickname);
+        newUser.setStatus("ACTIVE");
+        userRepository.save(newUser);
+        }
+        return ResponseEntity.ok().build();
+    }
 }
