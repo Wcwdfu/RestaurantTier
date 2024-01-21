4a21c287d445c1e8f9cd445f3c29a03037dc5332
imjaehyeong <dlawogud517@gmail.com>
2024-01-21 13:39:06 +0900

    home main image center alignment ongoing

--- src/test/java/com/site/restauranttier/RestaurantTierApplicationTests.java
+++ src/test/java/com/site/restauranttier/RestaurantTierApplicationTests.java
@@ -40,19 +40,19 @@ class RestaurantTierApplicationTests {
 //        restaurants.add(res1);
 //        restaurantRepository.saveAll(restaurants);
 
-        
-		// 상황 테스트 데이터 저장
-        Situation alone = new Situation(1,"혼밥");
-        Situation threeFour = new Situation(2,"2~4인");
-        Situation five = new Situation(3,"5인 이상");
-        Situation group  = new Situation(4,"단체 회식");
-        Situation delivery = new Situation(5,"배달");
-        Situation nightSnack = new Situation(6,"야식");
-        Situation invite = new Situation(7,"친구 초대");
-        Situation date = new Situation(8,"데이트");
-        Situation blindDate = new Situation(9,"소개팅");
-        List situationList = new ArrayList(Arrays.asList(alone,threeFour,five,group,delivery,nightSnack,invite,date,blindDate));
-        situationRepository.saveAll(situationList);
+//
+//		// 상황 테스트 데이터 저장
+//        Situation alone = new Situation(1,"혼밥");
+//        Situation threeFour = new Situation(2,"2~4인");
+//        Situation five = new Situation(3,"5인 이상");
+//        Situation group  = new Situation(4,"단체 회식");
+//        Situation delivery = new Situation(5,"배달");
+//        Situation nightSnack = new Situation(6,"야식");
+//        Situation invite = new Situation(7,"친구 초대");
+//        Situation date = new Situation(8,"데이트");
+//        Situation blindDate = new Situation(9,"소개팅");
+//        List situationList = new ArrayList(Arrays.asList(alone,threeFour,five,group,delivery,nightSnack,invite,date,blindDate));
+//        situationRepository.saveAll(situationList);
     }
 
 }
