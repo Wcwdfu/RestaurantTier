5f23e66c160fb5517bd9b7a4127233572b0185bc
imjaehyeong <dlawogud517@gmail.com>
2024-02-11 18:37:09 +0900

    tier and community design in home complete

---
+++ src/main/resources/static/js/homeScript.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const content = document.querySelector('#plate');
+    content.classList.add('on');
+
+    window.addEventListener('scroll', () => {
+        const content = document.querySelector('#plate');
+        if (window.innerHeight > content.getBoundingClientRect().top) {
+            content.classList.add('on');
+        }
+    });
+
+    window.addEventListener('scroll', () => {
+        const content = document.querySelector('#community');
+        if (window.innerHeight > content.getBoundingClientRect().top) {
+            content.classList.add('on');
+        }
+    });
+    window.addEventListener('scroll', () => {
+        const content = document.querySelector('#phrase');
+        if (window.innerHeight > content.getBoundingClientRect().top) {
+            content.classList.add('on');
+        }
+    });
+
+    window.addEventListener('scroll', () => {
+        const content = document.querySelector('#firstComment');
+        if (window.innerHeight > content.getBoundingClientRect().top) {
+            content.classList.add('on');
+        }
+    });
+    window.addEventListener('scroll', () => {
+        const content = document.querySelector('#secondComment');
+        if (window.innerHeight > content.getBoundingClientRect().top) {
+            content.classList.add('on');
+        }
+    });
+    window.addEventListener('scroll', () => {
+        const content = document.querySelector('#thirdComment');
+        if (window.innerHeight > content.getBoundingClientRect().top) {
+            content.classList.add('on');
+        }
+    });
+    window.addEventListener('scroll', () => {
+        const content = document.querySelector('#transferButton');
+        if (window.innerHeight > content.getBoundingClientRect().top) {
+            content.classList.add('on');
+        }
+    });
+
+});
