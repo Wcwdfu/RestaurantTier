c4189674a57248b4f68b4de4f46358641ca764bf
DingWoonee <slm0523@naver.com>
2024-02-19 05:08:54 +0900

    저장된 맛집 최신순으로 나열

--- src/main/java/com/site/restauranttier/controller/MypageController.java
+++ src/main/java/com/site/restauranttier/controller/MypageController.java
@@ -1,6 +1,8 @@
 package com.site.restauranttier.controller;
 
+import com.site.restauranttier.entity.Evaluation;
 import com.site.restauranttier.entity.Post;
+import com.site.restauranttier.entity.RestaurantFavorite;
 import com.site.restauranttier.entity.User;
 import com.site.restauranttier.repository.UserRepository;
 import com.site.restauranttier.service.CustomOAuth2UserService;
@@ -14,8 +16,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 @RequiredArgsConstructor
 @Controller
@@ -26,15 +27,24 @@ public class MypageController {
 
     @PreAuthorize("isAuthenticated() and hasRole('USER')")
     @GetMapping("/myPage")
-    public String myPage(Model model, Principal principal){
+    public String myPage(
+            @RequestParam(value = "menu-index", defaultValue = "0") int menuIndex,
+            Model model,
+            Principal principal
+    ){
         User user = customOAuth2UserService.getUser(principal.getName());
 
         model.addAttribute("user",user);
 
+        // 메뉴 탭 인덱스 정보
+        model.addAttribute("menuIndex", menuIndex);
         // 저장된 맛집 정보
+        List<RestaurantFavorite> favoriteList =  user.getRestaurantFavoriteList();
+        favoriteList.sort(Comparator.comparing(RestaurantFavorite::getCreatedAt).reversed());
         model.addAttribute("restaurantFavoriteList", user.getRestaurantFavoriteList());
         // 평가한 맛집 정보
-        model.addAttribute("restaurantEvaluationList", user.getEvaluationList());
+        // 나이를 기준으로 내림차순으로 정렬
+        model.addAttribute("restaurantEvaluationList", favoriteList);
 
         return "mypage";
     }
