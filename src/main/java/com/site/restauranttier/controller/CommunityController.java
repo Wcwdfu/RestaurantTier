4dcf63db22fefdae2731cccc2f5d2ad8d04016a5
imjaehyeong <dlawogud517@gmail.com>
2024-02-19 23:40:02 +0900

    삭제 에러 해결

--- src/main/java/com/site/restauranttier/controller/CommunityController.java
+++ src/main/java/com/site/restauranttier/controller/CommunityController.java
@@ -7,7 +7,9 @@ import com.site.restauranttier.entity.PostScrap;
 import com.site.restauranttier.entity.User;
 import com.site.restauranttier.repository.PostCommentRepository;
 import com.site.restauranttier.repository.PostRepository;
+import com.site.restauranttier.repository.PostScrapRepository;
 import com.site.restauranttier.service.*;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -36,7 +38,7 @@ public class CommunityController {
     private final PostRepository postRepository;
     private final PostCommentRepository postCommentRepository;
     private final PostScrapService postScrapService;
-
+    private final PostScrapRepository postScrapRepository;
     private static final Logger logger = LoggerFactory.getLogger(MainController.class);
 
     // 커뮤니티 메인 화면
@@ -85,6 +87,7 @@ public class CommunityController {
     }
     // 게시물 삭제
     @GetMapping("/api/post/delete")
+    @Transactional
     public ResponseEntity<String> postDelete(@RequestParam String postId) {
         Post post =  postService.getPost(Integer.valueOf(postId));
         //게시글 지워지면 그 게시글의 댓글들도 DELETED 상태로 변경
@@ -94,11 +97,14 @@ public class CommunityController {
         }
         //게시글 지워지면 그 게시글의 scrab정보들도 다 지워야함
         List<PostScrap> scraps = post.getPostScrapList();
-        scraps.clear();
+        postScrapRepository.deleteAll(scraps);
 
+
+
+        // 글 삭제
         post.setStatus("DELETED");
+
         postRepository.save(post);
-        System.out.println("User's postScrapList size after deletion: " + post.getPostScrapList().size());
         return ResponseEntity.ok("post delete complete");
     }
 
