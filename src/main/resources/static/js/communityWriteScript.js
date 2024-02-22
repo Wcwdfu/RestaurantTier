ddebeaf1e0ff943f041b4c56418233067da4cac4
imjaehyeong <dlawogud517@gmail.com>
2024-02-22 21:58:56 +0900

    버그 수정 완료

--- src/main/resources/static/js/communityWriteScript.js
+++ src/main/resources/static/js/communityWriteScript.js
@@ -1,5 +1,5 @@
-document.addEventListener('DOMContentLoaded', function () {
 
+document.addEventListener('DOMContentLoaded', function () {
     // 뒤로 가기 버튼에 이벤트 리스너 추가
     var backButton = document.getElementById('back-button');
     if (backButton) {
@@ -7,30 +7,40 @@ document.addEventListener('DOMContentLoaded', function () {
             window.history.back();
         });
     }
-    
-    // 작성 완료 버튼 클릭 리스너
+
+    // 작성 완료 버튼에 이벤트 리스너 추가 (추가적인 로직이 필요하다면 여기에 작성)
+    var completeButton = document.getElementById('complete-button');
+    if (completeButton) {
+        completeButton.addEventListener('click', function () {
+            // 작성 완료 관련 로직
+        });
+    }
+
     var form = document.querySelector('form');
     form.addEventListener('submit', function (event) {
+
+        event.preventDefault(); // 폼의 기본 제출 동작을 방지
         var category = form.querySelector('select[name="postCategory"]').value;
         if (!category) {
             alert('카테고리를 선택해주세요.');
             return;
         }
-        const formData = new FormData(form);
+
+        var formData = new FormData(form);
         fetch('/api/community/post/create', {
             method: 'POST',
             body: formData
         }).then(response => {
-            if (response.redirected)
+            if(response.redirected)
                 window.location.href = "/user/login";
             return response;
         })
             .then(data => {
-                window.location.href = "/community"
+                window.location.href="/community"
             }).catch(error => {
             alert(error.message)
             console.error('Error:', error);
-            window.location.href = "/community"
+            window.location.href="/community"
 
         });
     });
@@ -39,7 +49,7 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
-function openImageDialog() {
+function openImageDialog(){
     document.getElementById('imageInput').click();
 }
 
@@ -51,14 +61,14 @@ function insertImage() {
     }
 
     var reader = new FileReader();
-    reader.onloadend = function () {
+    reader.onloadend = function() {
         var base64Image = reader.result;
         console.log("Image Base64:", base64Image); // Check if the image is read correctly
         var textArea = document.querySelector('.post-content');
         textArea.value += '\n[img]' + base64Image + '[/img]\n'; // Append instead of replace
     };
 
-    reader.onerror = function (error) {
+    reader.onerror = function(error) {
         console.log("Error reading file:", error);
     };
 
