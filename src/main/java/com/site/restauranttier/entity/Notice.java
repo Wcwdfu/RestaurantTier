031fc2fa3dcc0f0cbff5491647d2a23c163749bd
kyung bo <skb9731@gmail.com>
2024-03-25 16:17:14 +0900

    공지사항 테이블,기능 추가

--- src/main/java/com/site/restauranttier/entity/Notice.java
+++ src/main/java/com/site/restauranttier/entity/Notice.java
@@ -1,7 +1,24 @@
 package com.site.restauranttier.entity;
 
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.sql.Date;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
 @Table(name = "notice_tbl")
 public class Notice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer noticeId;
+
+    String noticeTitle;
+    String noticeHref;
+    String status;
+    Date createdAt;
+    LocalDateTime updatedAt;
 }
